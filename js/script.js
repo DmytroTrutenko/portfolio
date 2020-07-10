6b00fc6fea9f25fd86335c2ee1279e5305b8b00e
DmytroTrutenko <55551711+DmytroTrutenko@users.noreply.github.com>
2020-07-10 20:52:02 +0300

    add scroll top, lang, img logo etc

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1) {
+            $('#header').addClass('header_scroll');
+            $('#header').removeClass('header');
+        } else {
+            $('#header').removeClass('header_scroll');
+            $('#header').addClass('header');
+        }
+    });
+
+    $("#main-nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+    $("#scroll_down").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
