d257553a7f68f7ffef9141b2bba8a3699c97925c
DmytroTrutenko <55551711+DmytroTrutenko@users.noreply.github.com>
2020-07-11 15:35:12 +0300

    parallax effect +

--- js/script.js
+++ js/script.js
@@ -22,7 +22,41 @@ $(document).ready(function () {
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1000);
     });
+    //при скролле увеличиваем opacity в мейн секции
+    $(window).on('scroll', function () {
+        var scrollCoef = 0.0004;
 
+        $('.main-screen').css({
+            opacity: 1 - $(window).scrollTop() * scrollCoef
+        })
+    });
+    //при скролле меняем положение bg text portfolio
+    $(window).on('scroll', function () {
+        var scrollCoef = 0.5;
+
+        $('.bg_text_port').css({
+            left:  50 + $(window).scrollTop() * scrollCoef,
+            transition: "all " + 1.5 + "s" + " ease-out"
+        })
+    });
+    //при скролле меняем положение bg text About Me
+    $(window).on('scroll', function () {
+        var scrollCoef = 0.07;
+
+        $('.bg_text_about').css({
+            left:  50 - $(window).scrollTop() * scrollCoef,
+            transition: "all " + 0.5 + "s" + " ease-out"
+        })
+    });
+    //при скролле меняем положение bg text what i do
+    $(window).on('scroll', function () {
+        var scrollCoef = 0.05;
+
+        $('.bg_text_ido').css({
+            left:  -150 + $(window).scrollTop() * scrollCoef,
+            transition: "all " + 0.5 + "s" + " ease-out"
+        })
+    });
 
     //перспектива на карточках
     var port_img = $(".port_img");
@@ -38,20 +72,19 @@ $(document).ready(function () {
 
     port_img.on('mouseenter', function () {
         $(this).children(".image").css({
-            transition: "all " + 0.05 + "s" + " linear"
+            transition: "all " + 0.3 + "s" + " ease-out"
         });
     });
 
     port_img.on('mouseleave', function () {
         $(this).children(".image").css({
-            transition: "all " + 0.2 + "s" + " linear"
+            transition: "all " + 0.2 + "s" + " ease-out"
         });
 
         $(this).children(".image").css("transform", "rotateY(" + 0 + "deg)" + " " + "rotateX(" + 0 + "deg)");
     });
 
-    function map(x, in_min, in_max, out_min, out_max)
-    {
+    function map(x, in_min, in_max, out_min, out_max) {
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
     }
 
